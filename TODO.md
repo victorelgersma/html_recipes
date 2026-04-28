411dc8c8a2dd1dcd01dcfdc25c78b677cf32d32a
Victor Elgersma <elgersmav@gmail.com>
2026-04-28 13:26:36 +0200

    todo

---
+++ TODO.md
@@ -0,0 +1,4 @@
+
+- [ ] search bar
+- [ ] build-time manifest for order of cards (instead of on every request)
+- [ ] avoid serving css-as-php
--- index.php
+++ index.php
@@ -13,22 +13,39 @@
             <?php
             $files = glob("html/*.html");
             if ($files) {
+                $with_images = [];
+                $without_images = [];
+
                 foreach ($files as $file) {
                     $slug = basename($file, ".html");
                     $title = ucwords(str_replace('-', ' ', $slug));
-
-                    // Construct URL for your external CDN
                     $thumbnail = "https://img.vjbe.net/$slug.webp";
 
-                    echo "
-        <a href='recipe.php?name=$slug' class='recipe-card'>
-            <div class='card-image' style='background-image: url(\"$thumbnail\"), url(\"img/placeholder.jpg\");'></div>
-            <div class='card-content'>
-                <h3>$title</h3>
-                <span class='view-link'>View Recipe →</span>
-            </div>
-        </a>";
+                    // Check if image exists. 
+                    // Note: @get_headers is a quick way to check external URLs, 
+                    // but it can be slow if you have 100+ recipes.
+                    $file_headers = @get_headers($thumbnail);
+                    $has_image = ($file_headers && strpos($file_headers[0], '200') !== false);
+
+                    $card_html = "
+            <a href='recipe.php?name=$slug' class='recipe-card'>
+                <div class='card-image' style='background-image: url(\"$thumbnail\"), url(\"img/placeholder.jpg\");'></div>
+                <div class='card-content'>
+                    <h3>$title</h3>
+                    <span class='view-link'>View Recipe →</span>
+                </div>
+            </a>";
+
+                    if ($has_image) {
+                        $with_images[] = $card_html;
+                    } else {
+                        $without_images[] = $card_html;
+                    }
                 }
+
+                // Output all recipes with images first
+                echo implode('', $with_images);
+                echo implode('', $without_images);
             }
             ?>
         </div>
